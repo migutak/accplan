be79671cc4c60e8403c3c7365c8d49d6bb3fd416
kevin abongo <kevinabo@ke.ibm.com>
2018-09-06 10:18:37 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    FacilitiesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/background/background.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.css']
+})
+export class BackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('app-background');
+  }
+
+  test() {
+    alert('me');
+  }
+
+}
